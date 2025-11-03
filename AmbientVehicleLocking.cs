7d1a320a0c6884490784203a4eb0b6fdbfdcae8d
Chris <36557217+bloolol@users.noreply.github.com>
2025-11-03 12:48:42 -0800

    Add files via upload

---
+++ AmbientVehicleLocking.cs
@@ -0,0 +1,71 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: AmbientVehicleLocking
+// Assembly: Enhanced Car Theft, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: B6C25BF0-B559-4433-A091-24E5A853B31D
+// Assembly location: C:\Users\clope\OneDrive\Desktop\Enhanced Car Theft.dll
+
+using GTA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class AmbientVehicleLocking : Script
+{
+    private Ped player;
+    private Vehicle currentVehicle;
+    private HashSet<Vehicle> lockedVehicles = new HashSet<Vehicle>();
+    private int lastCleanupTime = 0;
+    private Vehicle lastStolenVehicle = (Vehicle)null;
+
+    public AmbientVehicleLocking()
+    {
+        this.Tick += new EventHandler(this.OnTick);
+        this.Interval = 100;
+    }
+
+    private void OnTick(object sender, EventArgs e)
+    {
+        this.player = Game.Player.Character;
+        if ((Entity)this.player.CurrentVehicle != (Entity)null)
+        {
+            this.currentVehicle = this.player.CurrentVehicle;
+            if (!this.currentVehicle.IsStolen && (Entity)this.currentVehicle != (Entity)this.lastStolenVehicle)
+            {
+                this.currentVehicle.IsStolen = true;
+                this.currentVehicle.LockStatus = VehicleLockStatus.None;
+                this.lastStolenVehicle = this.currentVehicle;
+            }
+        }
+        if (EnhancedCarTheft.lockpickingIsEnabled)
+            this.LockNearbyAmbientVehicles();
+        if (Game.GameTime - this.lastCleanupTime < 5000)
+            return;
+        this.lastCleanupTime = Game.GameTime;
+        this.CleanupLockedVehicles();
+    }
+
+    private void LockNearbyAmbientVehicles()
+    {
+        foreach (Vehicle nearbyVehicle in World.GetNearbyVehicles(this.player.Position, 15f))
+        {
+            if (nearbyVehicle.Exists() && !((Entity)nearbyVehicle == (Entity)this.player.CurrentVehicle) && !nearbyVehicle.IsPersistent && !nearbyVehicle.IsTrain && !nearbyVehicle.IsPlane && !this.lockedVehicles.Contains(nearbyVehicle) && !nearbyVehicle.PreviouslyOwnedByPlayer && !nearbyVehicle.IsStolen && (Entity)nearbyVehicle.Driver == (Entity)null)
+            {
+                nearbyVehicle.LockStatus = VehicleHelper.VehicleIsValidType(nearbyVehicle) ? (RandomHelper.random.Next(100) <= 97 ? VehicleLockStatus.CannotEnter : VehicleLockStatus.None) : VehicleLockStatus.CanBeBrokenInto;
+                nearbyVehicle.NeedsToBeHotwired = true;
+                this.lockedVehicles.Add(nearbyVehicle);
+            }
+        }
+    }
+
+    private void CleanupLockedVehicles()
+    {
+        this.lockedVehicles.RemoveWhere((Predicate<Vehicle>)(veh => !veh.Exists() || veh.IsDead || veh.IsOnFire));
+        if (EnhancedCarTheft.lockpickingIsEnabled)
+            return;
+        foreach (Vehicle vehicle in this.lockedVehicles.ToList<Vehicle>())
+        {
+            vehicle.LockStatus = VehicleLockStatus.CanBeBrokenInto;
+            this.lockedVehicles.Remove(vehicle);
+        }
+    }
+}
